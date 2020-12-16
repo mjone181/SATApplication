c75487f247d6e189046148abee6112a49ef397d0
mjone181 <mjone181@gmail.com>
2020-12-16 08:51:19 -0600

    Add project files.

---
+++ SATApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SATApplication.Startup))]
+namespace SATApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
